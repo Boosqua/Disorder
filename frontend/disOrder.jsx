1c83ec5815451570a7785fa4567cb6d81654fdd6
Omar Hernandez <oah232@gmail.com>
2021-03-30 20:29:44 -0700

    delete package-lock.json

--- frontend/disOrder.jsx
+++ frontend/disOrder.jsx
@@ -2,12 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import configureStore from './store/store';
 import Root from './components/root'
-/* test */
-import {
-  login,
-  logout,
-  signup
-} from "./util/session_api_util"
+
 
 
 document.addEventListener("DOMContentLoaded", () => {
