019999041fc859414506d651f37523a80353c953
Omar Hernandez <oah232@gmail.com>
2021-03-31 18:00:11 -0700

    prepare for heroku push

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,21 +1,15 @@
 import React from "react";
-// import { createContext } from 'react';
 import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
-import actionCable from 'actioncable'
 import App from "./App";
 
-// const CableApp = {}
-// CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable')
-// export const ActionCableContext = React.createContext();
+
 const Root = ({ store }) => {
    return (
   <Provider store={store}>
-   {/* <ActionCableContext.provider value={CableApp.cable} > */}
       <HashRouter>
          <App />
       </HashRouter>
-    {/* </ActionCableContext.provider> */}
   </Provider>
 )};
 
