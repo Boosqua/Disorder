cad97d2355375ac606412fe14bfcd8721360c96e
Omar Hernandez <oah232@gmail.com>
2021-04-01 15:17:14 -0700

    fix delete action in backend MessageJobs class

--- app/jobs/message_job.rb
+++ app/jobs/message_job.rb
@@ -3,16 +3,16 @@ class MessageJob < ApplicationJob
 
   def perform(id)
       data = Message.find(id)
-      new_message = { 
-         id: data["id"],
-         author_id: data['author_id'], 
-         body: data['body'], 
-         imageable_id: data['imageable_id'],
-         imageable_type: data["imageable_type"]
-      }
-      new_message[:photoUrl] =  data.photo.attached?
-      ActionCable.server.broadcast("server_#{new_message[:imageable_type]}#{new_message[:imageable_id]}", new_message)
-
-
+      if !!data 
+         new_message = { 
+            id: data["id"],
+            author_id: data['author_id'], 
+            body: data['body'], 
+            imageable_id: data['imageable_id'],
+            imageable_type: data["imageable_type"]
+         }
+         new_message[:photoUrl] =  data.photo.attached?
+         ActionCable.server.broadcast("server_#{new_message[:imageable_type]}#{new_message[:imageable_id]}", new_message)
+      end
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,11 +2,12 @@ class Message < ApplicationRecord
    belongs_to :imageable, polymorphic: true
    belongs_to :author, class_name: :User
    has_one_attached :photo
-   after_commit {MessageJob.perform_now self.id}
+   after_create_commit {MessageJob.perform_now self.id}
    #update friends last message time if friend message
    before_create do
       if self.imageable_type == "Friend" 
          Friend.find(self.imageable_id).update( last_message: Time.new.to_i )
       end
+      
    end
 end
